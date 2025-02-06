566f4f100854eb2dd0a930edf5f9b776f3720471
Karl C <kjacand02+github@gmail.com>
2025-02-06 12:34:17 -0500

    Update to date() chapter

--- learning/script.js
+++ learning/script.js
@@ -717,3 +717,32 @@ veg.forEach(veg => console.log(veg.color));
 console.log(vegNames);
 console.log(loCal);
 */
+/*Array Shuffling
+const cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10,'J', 'Q', 'K'];
+
+
+function shuffle(array){
+    for(let i = array.length - 1; i > 0; i--){
+        const random = Math.floor(Math.random() * (i + 1));
+    
+        [array[i], array[random]] = [array[random], array[i]]
+    } 
+}
+shuffle(cards);
+
+console.log(cards);
+*/
+/* Date()
+const date = new Date();
+
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+
+date.setMonth(9);
+
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(date);
+*/
